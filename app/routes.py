24eaaed2e5eb6cc18d786198b4e86ea86df8983e
achref <achrefbs>
2020-10-29 20:13:50 +0100

    add course

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,9 @@ import os
 @app.route('/')
 @app.route('/home')
 def index():
-	return render_template('index.html', title='YouLearn')
+	courses = db.session.query(Course).all()
+	count = db.session.query(Course).count()
+	return render_template('index.html', title='YouLearn', courses=courses, count=count)
 
 @app.route('/settings')
 def settings():
@@ -95,5 +97,29 @@ def displayCategory(categoryId):
 def categories():
 	categories = db.session.query(Category).all()
 	count = db.session.query(Category).count()
-	print(count)
 	return render_template('categories.html', categories=categories, count=count)
+
+@app.route("/courses")
+def displaycourses():
+	courses = db.session.query(Course).all()
+	count = db.session.query(Course).count()
+	return render_template('courses.html', courses=courses, count=count)
+
+
+
+
+
+@app.route("/courses/new", methods=['GET', 'POST'])
+def NewCourse():
+	form = addCourseForm()
+	form.category.choices = [(row.categoryid, row.category_name) for row in Category.query.all()]
+	if form.validate_on_submit():
+		image = images.save(form.image.data)
+		course = Course(course_name=form.CourseName.data, description=form.CourseDescription.data, discounted_price=15, regular_price=form.CoursePrice.data, image=image)
+		db.session.add(course)
+		db.session.commit()
+		course_category = CourseCategory(categoryid=form.category.data, courseid=course.courseid)
+		db.session.add(course_category)
+		db.session.commit()
+		return redirect(url_for('index'))
+	return render_template("addCourse.html", form=form, legend="New Course")
